ca66f085605ea1e3206e92fc5f72f5ff981f2c56
cyberalex <bayanalex@gmail.com>
2022-11-27 02:33:43 +0200

    feat: made testimonials carousel and modal popup feat: made modal for burger menu

--- script.js
+++ script.js
@@ -51,28 +51,82 @@ const petsCardsData = [
 let petsCarouselScrolling = false;
 let petsCarouselItemsCount = 6;
 let mediaSmallDesktop = window.matchMedia('(max-width: 999px)');
+let mediaTablet = window.matchMedia('(max-width: 639px)');
 
-document.querySelectorAll('.main-menu__item').forEach(item => item.onclick = !item.classList.contains('main-menu__item_highlighted') ? hideHighlitedMenuItem : undefined);
-petsCarouselInit();
+init();
+
+function init() {
+    mediaSmallDesktop.addEventListener('change', () => {
+        pertsCarouselInitCards();
+    });
+    
+    document.querySelectorAll('.main-menu__item').forEach(item => item.onclick = !item.classList.contains('main-menu__item_highlighted') ? hideHighlitedMenuItem : undefined);
+    petsCarouselInit();
+    testimonialsCarouselInit();
+    document.querySelector('.menu-checkbox').addEventListener('change', menuChange);
+
+    document.querySelectorAll('.testimonial-card').forEach(card => card.addEventListener('click', showTestimonialPopup));
+}
+
+function menuChange(event) {
+    const checkbox = document.querySelector('.menu-checkbox');
+    if(checkbox.checked == true) {
+        const modal = addModal();
+        modal.addEventListener('click', () => {
+            checkbox.checked = false;
+            checkbox.dispatchEvent(new Event('change'));
+        });
+        disableScroll();
+    }
+    else {
+        removeModal();
+        enableScroll();
+    }
+}
+
+function addModal() {
+    document.body.insertAdjacentHTML('beforeend', '<div class="modal"></div>');
+    const modal = document.querySelector('.modal');
+    setTimeout(() => modal.classList.add('modal_show'), 0);
+    return modal;
+}
+
+function removeModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.remove('modal_show');
+    modal.addEventListener('transitionend', () => modal.remove());
+}
+
+function disableScroll() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    window.onscroll = () => {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
+}
+
+function enableScroll() {
+    window.onscroll = '';
+}
 
 function hideHighlitedMenuItem() {
     if(!this.classList.contains('.main-menu__item_highlighted'))
         document.querySelectorAll('.main-menu__item_highlighted').forEach(item => item.classList.remove('main-menu__item_highlighted'));
 }
 
+//*** PETS CAROUSEL ***
+
 function petsCarouselInit() {
+    pertsCarouselInitCards();
+    document.querySelectorAll('.pets__arrow').forEach(arrow => arrow.addEventListener('click', petsCarouselClick));
+}
+
+function pertsCarouselInitCards() {
     petsCarouselItemsCount = mediaSmallDesktop.matches ?  4 : 6;
     const carousel = document.querySelector('.pet-carousel');
+    carousel.querySelectorAll('.pet-cards').forEach(cards => cards.remove());
     carousel.append(petsCarouselCreateCards());
     carousel.querySelectorAll('.pet-card').forEach(card => card.classList.add('pet-card_hover'));
-    document.querySelectorAll('.pets__arrow').forEach(arrow => arrow.addEventListener('click', petsCarouselClick));
-
-    mediaSmallDesktop.addEventListener('change', (event) => {
-        location.reload();
-        // carousel.querySelectorAll('.pet-cards').forEach(card => card.remove());
-        // petsCarouselItemsCount = event.target.matches ? 4 : 6;
-        // carousel.append(petsCarouselCreateCards());
-    });
 }
 
 function petsCarouselCreateCards() {
@@ -102,7 +156,7 @@ function petsCarouselClick(event) {
     let carouselScreen = petsCarouselCreateCards();
     const carousel = document.querySelector('.pet-carousel');
     carousel.querySelectorAll('.pet-card').forEach(card => card.classList.remove('pet-card_hover'));
-    if(event.currentTarget.classList.contains('pets__arrow_right')) {
+    if(event.currentTarget.classList.contains('pets__arrow_left')) {
         carousel.classList.add('pet-carousel_left-hidden');
         carousel.prepend(carouselScreen);
         carousel.addEventListener('transitionend', () => {
@@ -116,7 +170,7 @@ function petsCarouselClick(event) {
             carousel.classList.remove('pet-carousel_left-hidden');
         }, 0);
 
-    } else if(event.currentTarget.classList.contains('pets__arrow_left')) {
+    } else if(event.currentTarget.classList.contains('pets__arrow_right')) {
         carousel.append(carouselScreen);
         carousel.addEventListener('transitionend', () => {
             carousel.firstElementChild.remove();
@@ -145,4 +199,57 @@ function petsCreateCard(index) {
                     </div>
                 </div>
             </div>`;
+}
+
+//*** TESTIMONIALS ***
+
+function testimonialsCarouselInit() {
+    const scroll = document.querySelector('.testimonials-block__scroll');
+    scroll.addEventListener('input', testimonialsCarouselChange);
+}
+
+function testimonialsCarouselChange(event) {
+    const cards = document.querySelector('.testimonials-cards');
+    const gap = parseInt(getComputedStyle(cards).columnGap);
+    const cardWidth = parseInt(getComputedStyle(document.querySelector('.testimonial-card')).width);
+    cards.style.left = -event.target.value * (cardWidth + gap) + 'px';
+}
+
+function showTestimonialPopup(event) {
+    if(!window.matchMedia('(max-width: 800px)').matches)
+        return;
+    const modal = addModal();
+    const currentCard = event.currentTarget;
+    const popup = currentCard.cloneNode(true);
+    let savedStyle = '';
+    modal.addEventListener('click', closePopup);
+    modal.style.zIndex = 5;
+    popup.classList.add('testimonial-card_popup');
+    const closeButton = document.createElement('img');
+    closeButton.classList.add('testimonial-card__close-button');
+    closeButton.src = 'assets/icons/x_icon.svg';
+    closeButton.addEventListener('click', closePopup);
+    popup.querySelector('.testimonial-card__content').append(closeButton);
+    const style = getComputedStyle(currentCard);
+    popup.style.top = currentCard.getBoundingClientRect().top + 'px';
+    popup.style.left = currentCard.getBoundingClientRect().left + 'px';
+    popup.style.width = style.width;
+    popup.style.height = style.height;
+    savedStyle = popup.getAttribute('style');
+    document.body.append(popup);
+    disableScroll();
+    setTimeout(() => {
+        popup.removeAttribute('style');
+        popup.classList.add('testimonial-card_popup_show');
+    }, 0, {once: true});
+
+    function closePopup() {
+        popup.setAttribute('style', savedStyle);
+        removeModal();
+        popup.classList.remove('testimonial-card_popup_show');
+        popup.addEventListener('transitionend', () => {
+            popup.remove();
+            enableScroll();
+        });
+    }
 }
